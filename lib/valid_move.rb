319afafcecff351f92d1b0e7b4a9f5c79b33c61d
fred.burgess <fred.burgess@whitehat.org.uk>
2019-08-07 16:19:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,8 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, idx)
   case board[idx]
-  
+    when "X" then return true
+    when "O" then return true
   end
+  false
 end
